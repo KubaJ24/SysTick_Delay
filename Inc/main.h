f221927dec0117a3a5469b8728d47263a0cc9bac
KubaJ24 <j.jarosz@onet.pl>
2023-12-01 13:52:17 +0100

    Empty project

---
+++ Inc/main.h
@@ -0,0 +1,14 @@
+/*
+ * main.h
+ *
+ *  Created on: Nov 19, 2023
+ *      Author: ahadz
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdint.h>
+#include "stm32f746xx.h"
+
+#endif /* MAIN_H_ */
